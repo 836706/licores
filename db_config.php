0085dab9688551b479bca438bf788b8b50697523
836706 <169112500+836706@users.noreply.github.com>
2024-05-27 20:28:21 -0500

    Add files via upload

--- db_config.php
+++ db_config.php
@@ -1,8 +1,6 @@
 
 <?php
-// Conexión a la base de datos (debes reemplazar los valores con los de tu base de datos)
-// si tienes xamp y tu usuario es root y no tienes contraseña dejarlo como esta sin modificar
-// si tu usuario es root pero tiene clave modifcar el $password ="tu-clave";
+
 $servername = "localhost";
 $username = "root";
 $password = "";
